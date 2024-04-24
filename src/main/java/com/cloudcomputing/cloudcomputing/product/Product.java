df0e5e14fca542907afc3233752c0bc73e53bddf
congdunghzz <lebaoduy023502@gmail.com>
2024-04-24 11:51:18 +0700

    first commit

---
+++ src/main/java/com/cloudcomputing/cloudcomputing/product/Product.java
@@ -0,0 +1,36 @@
+package com.cloudcomputing.cloudcomputing.product;
+
+import com.cloudcomputing.cloudcomputing.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+
+    @Id
+    @Column(name = "PRODUCT_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "PRODUCT_NAME")
+    private String name;
+
+    @Column(name = "PRODUCT_PRICE")
+    private double price;
+
+    @Column(name = "PRODUCT_STOCK")
+    private int stock;
+
+    @Column(name = "PRODUCT_IMAGE")
+    private String image;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn (name = "CATEGORY_ID")
+    private Category category;
+}
